721071216cf1f64ac962f6a3c21a541da403a4c8
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-01-31 11:57:52 -0500

    gitignore added

--- team_solutions/pauley_x/circuits_with_bloqade_test.py
+++ team_solutions/pauley_x/circuits_with_bloqade_test.py
@@ -12,3 +12,19 @@ from bloqade import squin
 
 Register = IList[Qubit, Any]
 
+@squin.kernel
+def hello_world(theta: float) -> IList[MeasurementResult, Any]:
+    """
+    Prepare a Bell state and measure in a basis that might have a Bell violation
+    """
+    qubits = squin.qalloc(2)
+    squin.h(qubits[0])
+    squin.cx(qubits[0], qubits[1])
+    squin.rx(theta, qubits[0])
+    bits = squin.broadcast.measure(qubits)
+    return bits
+
+
+# [kernel].print() prints the raw SSA, which is the intermediate representation of the kernel
+# as used internally by Kirin.
+# hello_world.print()
