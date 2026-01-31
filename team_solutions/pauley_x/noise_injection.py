ee8597fb77947d83d2dc5a207cf8b01126f9f0bc
Ethan Hopkins <ethan.e.hopkins@gmail.com>
2026-01-31 18:35:40 -0500

    wip tsim backend

--- team_solutions/pauley_x/noise_injection.py
+++ team_solutions/pauley_x/noise_injection.py
@@ -22,16 +22,25 @@ def _cirq_gemini_two_zone_noise(circuit: cirq.Circuit, scaling_factor: float = 1
     return noisy_circuit
 
 
-def analyze_noise_channels(ker, noise_model):
+def analyze_noise_channels(ker, noise_source, noise_model = _cirq_gemini_two_zone_noise):
     """Tests importance of different noise channels"""
     
-    clean_circuit = cirq_utils.emit_circuit(ker)
-    noisy_circuit = transform_circuit(clean_circuit, noise_model)
+    ### Uses a Cirq backend
+    if noise_source == "cirq_heuristic_model":
+        clean_circuit = cirq_utils.emit_circuit(ker)
+        noisy_circuit = transform_circuit(clean_circuit, noise_model)
+        simulator = cirq.DensityMatrixSimulator()
+        result = simulator.simulate(noisy_circuit)
+        return result
+    
+    ### Uses a QuEra tsim backend
+    if noise_source == "custom":
+        clean_circuit = ker
+        sampler = clean_circuit.compile_sampler()
+        samples = sampler.sample(shots=100)
+        return samples
 
-    simulator = cirq.DensityMatrixSimulator()
-    result = simulator.simulate(noisy_circuit)
-
-    return result
+    raise ValueError("Unknown parameters")
 
 ker = None # Replace with Pranav's stuff
 print(analyze_noise_channels(ker, _cirq_gemini_two_zone_noise))
